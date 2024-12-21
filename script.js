546f35b9e695e479f0ddaf36a20a475bb7b7e79c
Sephydev <nicolas.bussiere@outlook.fr>
2024-12-21 14:28:50 +0100

    Create 16 * 16 cell with js

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+const numOfCell = 16 * 16;
+
+for (let i = 0; i < numOfCell; i++) {
+    let cell = document.createElement("div");
+    cell.setAttribute('class', 'cell');
+    container.appendChild(cell);
+}

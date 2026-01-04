07ee0660b477b560475fad1989402cafcec79381
Sephydev <nicolas.bussiere@outlook.fr>
2026-01-04 18:41:54 +0100

    feat(etch-a-sketch): Enable to change size of grid

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 const container = document.querySelector(".container");
+const changeSizeBtn = document.querySelector(".change-size");
 
-for (i = 0; i < (16); i++) {
-  const row = document.createElement("div");
-  row.className = "row";
-  
-  for (j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    row.appendChild(square);
+let size = 16;
+
+function displayGrid (size) {
+  for (i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+
+    const squareSize = Math.floor((container.offsetWidth - size) / size);
+    
+    for (j = 0; j < size; j++) {
+      const square = document.createElement("div");
+      square.className = "square";
+
+      square.style.width = `${squareSize}px`;
+      square.style.height = `${squareSize}px`;
+
+      row.appendChild(square);
+    }
+
+    container.appendChild(row)
+
   }
-  container.appendChild(row)
 }
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach(square => {
-  square.addEventListener("mouseenter", () => {
-    square.classList.add("hover");
+function hoverSquare () {
+  const squares = document.querySelectorAll(".square");
+  
+  squares.forEach(square => {
+    square.addEventListener("mouseenter", () => {
+      square.classList.add("hover");
+    })
   })
+}
+
+changeSizeBtn.addEventListener("click", () => {
+  do {
+    size = Number(prompt("Which size do you want?"));
+  } while (isNaN(size) || size <= 0 || size > 100)
+    
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+    
+  displayGrid(size);
+  hoverSquare();
 })
+  
+  
+displayGrid(size);
+hoverSquare();
